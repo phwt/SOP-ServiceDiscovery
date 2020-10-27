28b9fd27c8a84e5684b8cca5c3636eea87f8fa96
phwt.smwt <phwt.smwt@gmail.com>
2020-10-27 17:51:30 +0700

    Allow Cross Origin

--- services/service-discovery-server/src/main/java/com/microservices/servicediscovery/App.java
+++ services/service-discovery-server/src/main/java/com/microservices/servicediscovery/App.java
@@ -3,11 +3,28 @@ package com.microservices.servicediscovery;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
+@SuppressWarnings("deprecation")
 @SpringBootApplication
 @EnableEurekaServer
+@CrossOrigin
 public class App {
     public static void main( String[] args ) {
         SpringApplication.run(App.class, args);
+    }
+    
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+       return new WebMvcConfigurerAdapter() {
+          @Override
+          public void addCorsMappings(CorsRegistry registry) {
+             registry.addMapping("/**").allowedOrigins("*");
+          }
+       };
     }
 }
